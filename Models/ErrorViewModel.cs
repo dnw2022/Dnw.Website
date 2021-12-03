404c65ef791a30d0134da7c29a1678c9f5b90614
jbijlsma <jeroen_bijlsma@yahoo.com>
2021-12-03 08:00:33 +0100

    Fix warnings

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace Dnw.Website.Models;
-
-public class ErrorViewModel
+namespace Dnw.Website.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
